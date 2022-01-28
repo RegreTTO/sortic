33546ec7d3bc61a02f3fb616e7858d961ad6890d
Gdebtd <76738374+Gdebtd@users.noreply.github.com>
2022-01-28 20:48:06 +0300

    Update sortic.h

--- Scripts/sortic/sortic.h
+++ Scripts/sortic/sortic.h
@@ -17,5 +17,8 @@ int parse_number(string str);
 // algorithm
 void print_vectors(vector <int> & a, vector <int> & b, string function_name);
 void sort_algorithm(vector <int> & a, vector <int> & b, string & str);
+int min_ind(vector <int> & a);
+int max_ind(vector <int> & a);
+int char_cnt(string str, char ch);
 
 #endif
