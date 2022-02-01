14b5dd6f6afcd4a28b793bfa7abe02accdd4b7ba
RegreTTO <atary_game@mail.ru>
2022-02-01 20:24:05 +0300

    color constants added

---
+++ Scripts/colors/colors.cpp
@@ -0,0 +1,4 @@
+#include "colors.h"
+
+const char *RED = "\033[31m";
+const char *BLUE = "\033[34m";
---
+++ Scripts/colors/colors.h
@@ -0,0 +1,7 @@
+#ifndef SORTIC_COLORS_H
+#define SORTIC_COLORS_H
+
+extern const char* RED;
+extern const char* BLUE;
+
+#endif //SORTIC_COLORS_H
